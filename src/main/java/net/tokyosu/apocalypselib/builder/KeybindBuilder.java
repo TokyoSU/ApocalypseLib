98466d17891734a66b653835b003c5413ce60fac
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2026-02-10 12:48:52 +0100

    Rollback InputCategory.

--- src/main/java/net/tokyosu/apocalypselib/builder/KeybindBuilder.java
+++ src/main/java/net/tokyosu/apocalypselib/builder/KeybindBuilder.java
@@ -85,4 +85,25 @@ public class KeybindBuilder {
     public KeyMapping build() {
         return new KeyMapping(this.resource_name, this.context, InputConstants.getKey(this.key, -1), this.category.getName());
     }
+
+    /**
+     * Key category in-game as enum.
+     */
+    public enum InputCategory {
+        CREATIVE(KeyMapping.CATEGORY_CREATIVE),
+        UI(KeyMapping.CATEGORY_INTERFACE),
+        GAMEPLAY(KeyMapping.CATEGORY_GAMEPLAY),
+        INVENTORY(KeyMapping.CATEGORY_INVENTORY),
+        MISC(KeyMapping.CATEGORY_MISC),
+        MOVEMENT(KeyMapping.CATEGORY_MOVEMENT),
+        MULTIPLAYER(KeyMapping.CATEGORY_MULTIPLAYER);
+
+        private final String name;
+
+        public String getName() { return name; }
+
+        InputCategory(String category) {
+            this.name = category;
+        }
+    }
 }
